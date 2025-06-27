995cc131d7785632fb71de1c002dc076ef36a564
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-06-27 12:17:05 +0900

    0627_1216

--- src/components/FrontEndResult.tsx
+++ src/components/FrontEndResult.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { Checkbox, Container, Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react"
+import { Checkbox, Container, Link, Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react"
 import { CheckboxGroup } from "@chakra-ui/react";
 import { TodoTitle } from "./TodoTitle";
 import { Tabs } from "@chakra-ui/react";
 import { Paper } from "@mui/material";
 import { AddIcon } from "@chakra-ui/icons";
 import { SideMenu } from "./SideMenu";
+import { Text } from "@chakra-ui/react"
 // ルーティング設定に必要なものをimport
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
@@ -28,15 +29,21 @@ import { Bar } from 'react-chartjs-2'
 import 'chart.js/auto' // 今回はバンドルサイズ気にしないので、autoを使う
 import SideMenu_temp from "./SideMenu_temp";
 import SideMenu_top from "./SideMenu_top";
-
-
+import { BlobProvider, BlobProviderParams, DocumentProps, usePDF } from "@react-pdf/renderer";
+import PDF from "./PDF";
+import React from "react";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 const FrontEndResult = () => {
 
     const location = useLocation();
     let navigate = useNavigate();
-    const level1_Array: number[] = location.state.level1;
-    const level2_Array: number[] = location.state.level2;
-    const level3_Array: number[] = location.state.level3;
+    const level1_Array: string[] = location.state.level1;
+    const level2_Array: string[] = location.state.level2;
+    const level3_Array: string[] = location.state.level3;
+
+    console.log('基礎レベル（レベル1）', location.state.level1);
+    console.log('現場レベル（レベル2）', location.state.level2);
+    console.log('応用レベル（レベル3）', location.state.level3);
 
     //4,3,2,1だったら、[4,3,2,1]の文字数：11
     //3,1,2だったら、[3,1,2]：7
@@ -49,13 +56,37 @@ const FrontEndResult = () => {
     const lv2 = Object.keys(level2_Array).length;
     const lv3 = Object.keys(level3_Array).length;
 
+    const level1_Array_content = [...level1_Array];
+    console.log('内容です1', level1_Array)
+    console.log('内容です2', level1_Array_content)
+
+    //削除＆追加
+    const viewPdf = async (name: any) => {
+        navigate("/viewPdf/")
+    };
+    // interface OnRenderProps {
+    //     blob?: Blob;
+    //     loading: boolean;
+    //     error: Error | null;
+    // }
+    // interface BlobProviderProps {
+    //     document: React.ReactElement<DocumentProps>;
+    //     children: (params: BlobProviderParams) => React.ReactNode;
+    // }
+    // interface UsePDFInstance {
+    //     loading: boolean;
+    //     blob: Blob | null;
+    //     url: string | null;
+    //     error: string | null;
+    // }
+
     const data = {
         // x 軸のラベル
         labels: ['基礎レベル', '現場レベル', '上級レベル'],
         datasets: [
             {
 
-                label: 'Dataset',
+                label: 'Language分野',
                 // データの値
                 data: [lv1, lv2, lv3],
                 // グラフの背景色
@@ -77,6 +108,22 @@ const FrontEndResult = () => {
     };
 
 
+    const [instance, updateInstance] = usePDF({ document: <PDF /> });
+    if (instance.loading) return <div>Loading ...</div>;
+
+    if (instance.error) return <div>Something went wrong: {instance.error}</div>;
+    console.log('インスタンス', instance);
+    console.log('URL', instance.url);
+    //「instance.url」データ型：ReactPDF.UsePDFInstance.url: string | null
+    //string | null 型を、string | undefined型に代入しようとしているのでエラー
+
+    //String型へ型変換
+    const url_j = instance.url as string;
+
+    console.log('ローディング', instance.loading);
+    console.log('エラー', instance.error);
+    console.log('BLOG', instance.blob)
+
     let goBack = () => {
         navigate(-1)
     };
@@ -85,18 +132,55 @@ const FrontEndResult = () => {
         navigate("/graph/")
     }
     return (
+
         <>
+
             <ChakraProvider theme={theme}>
                 <Flex w="100vw" h="100wh">
                     <SideMenu_top />
                     <SideMenu_temp />
 
-                    <Flex w="50vw" h="50wh">
+                    <Flex w="50vw" h="50wh" direction={"column"}>
                         <Bar data={data} />
+                        <br />
+                        <Text bg='rgb(255, 99, 132)'>基礎レベル<b>[{level1_Array.length}]</b>ポイント</Text>
+                        {level1_Array.map((content: any, index: any) => (
+                            <Flex direction="column">
+                                {content}
+                            </Flex>
+                        ))}
+                        <br />
+                        <Text bg='rgb(255, 159, 64)'>現場レベル<b>[{level2_Array.length}]</b>ポイント</Text>
+                        {level2_Array.map((content: any, index: any) => (
+                            <Flex direction="column">
+                                {content}
+                            </Flex>
+                        ))}
+                        <br />
+                        <Text bg='rgb(75, 192, 192)'>応用レベル<b>[{level3_Array.length}]</b>ポイント</Text>
+                        {level3_Array.map((content: any, index: any) => (
+                            <Flex direction="column">
+                                {content}
+                            </Flex>
+                        ))}
+                        <br />
+                        <br />
+                        <br />
+                        <Flex>
+                            <Button colorScheme="yellow" size="sm" variant="outline" leftIcon={<AddIcon />} onClick={() => viewPdf('dummy')}>PDF表示</Button>
+
+                            <Button colorScheme="blue" size="sm" variant="outline" leftIcon={<AddIcon />}>
+                                <Link href={url_j} download="test.pdf">PDFダウンロード</Link>
+                            </Button>
+
+
+
+                        </Flex>
 
                     </Flex>
 
                 </Flex>
+
                 <Button
                     onClick={goBack}
                     colorScheme="green"
@@ -104,6 +188,8 @@ const FrontEndResult = () => {
                     mt="8">戻る
                 </Button>
             </ChakraProvider>
+
+
 
         </>
 
---
+++ src/components/PDF.tsx
@@ -0,0 +1,186 @@
+import { Font, Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import notoRegular from '../fonts/NotoSansJP-Regular.ttf';
+import notoBold from '../fonts/NotoSansJP-Bold.ttf'
+
+//React-pdfが提供するコンポーネントやAPIを利用してPDFのスタイルを定義
+Font.register({
+  family: 'font_001',
+  fonts: [
+    {
+      src: notoRegular as string,
+    },
+    {
+      src: notoBold as string,
+      fontWeight: 'bold',
+    },
+  ],
+})
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 30,
+    fontSize: 11,
+    fontFamily: "font_001",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 0,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  details: {
+    marginBottom: 30,
+  },
+  detailItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemsTable: {
+    display: "flex",
+    width: "auto",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+  },
+  tableRow: {
+    margin: "auto",
+    flexDirection: "row",
+  },
+  tableColHeader: {
+    width: "20%",
+    borderStyle: "solid",
+    borderWidth: 1,
+    textAlign: "center",
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+    fontWeight: "bold",
+    padding: 5,
+  },
+  tableCol: {
+    width: "20%",
+    borderStyle: "solid",
+    borderWidth: 1,
+    textAlign: "center",
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+    padding: 5,
+  },
+  textVertical: {
+    flexDirection: "column",
+  },
+  company: {
+    marginTop: 10,
+  },
+});
+
+const data = [
+  {
+    title: "発注日",
+    value: "2024/4/01",
+    items: [
+      {
+        name: "サンプル1",
+        surface: "1",
+        thickness: "式",
+        width: "10,000",
+        length: "10,000",
+      },
+      {
+        name: "サンプル1",
+        surface: "1",
+        thickness: "式",
+        width: "10,000",
+        length: "10,000",
+      },
+      {
+        name: "サンプル1",
+        surface: "1",
+        thickness: "式",
+        width: "10,000",
+        length: "10,000",
+      },
+      {
+        name: "サンプル1",
+        surface: "1",
+        thickness: "式",
+        width: "10,000",
+        length: "10,000",
+      },
+      {
+        name: "サンプル1",
+        surface: "1",
+        thickness: "式",
+        width: "10,000",
+        length: "10,000",
+      },
+      {
+        name: "サンプル1",
+        surface: "1",
+        thickness: "式",
+        width: "10,000",
+        length: "10,000",
+      },
+    ],
+  },
+];
+
+export default function PDF() {
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View>
+          <Text style={styles.header}>領収書</Text>
+        </View>
+        <View style={styles.details}>
+          {data.map((detail, index) => (
+            <View style={styles.detailItem} key={index}>
+              <View style={styles.textVertical}>
+                <Text>株式会社〇〇</Text>
+                <Text>〇〇 御中</Text>
+              </View>
+              <View>
+                <Text>発行日{detail.value}</Text>
+                <View style={styles.company}>
+                  <Text>株式会社〇〇</Text>
+                  <Text>東京都〇〇〇〇〇〇〇〇〇〇</Text>
+                  <Text>TEL：00-0000-0000</Text>
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
+        <View>
+          <View style={styles.itemsTable}>
+            <View style={styles.tableRow}>
+              {/* 大項目
+                    フロントエンド
+                    バックエンド
+                    インフラ
+                    */}
+              <Text style={styles.tableColHeader}>大項目</Text>
+              <Text style={styles.tableColHeader}>中項目</Text>
+              <Text style={styles.tableColHeader}>単位</Text>
+              <Text style={styles.tableColHeader}>単価</Text>
+              <Text style={styles.tableColHeader}>金額</Text>
+            </View>
+            {data[0].items.map((item, index) => (
+              <View style={styles.tableRow} key={index}>
+                <Text style={styles.tableCol}>{item.name}</Text>
+                <Text style={styles.tableCol}>{item.surface}</Text>
+                <Text style={styles.tableCol}>{item.thickness}</Text>
+                <Text style={styles.tableCol}>{item.width}</Text>
+                <Text style={styles.tableCol}>{item.length}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+}
