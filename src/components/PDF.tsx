dc2300f49898a29844033865c67b7ba56c8d091d
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-02 23:32:57 +0900

    0702_2332

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -212,7 +212,7 @@ export default function PDF() {
               <Text style={styles.tableColHeader}>金額</Text>
             </View>
 
-            {front_language1.map((content: any, index: any) => (
+            {data.map((content: any, index: any) => (
               <View style={styles.tableRow} key={index}>
                 <Text style={styles.tableCol}>{content}</Text>
                 <Text style={styles.tableCol}>{content}</Text>
