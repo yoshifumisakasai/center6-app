92473ba393e0078ae6a847eb04de7fce3d7e5865
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-05 23:52:58 +0900

    0725_2350

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -203,7 +203,6 @@ export default function PDF() {
               <Text style={styles.tableColHeader}>単価</Text>
               <Text style={styles.tableColHeader}>金額</Text>
             </View>
-            <p>{countRef.current}</p>
             {countRef.current.map((content: any, index: any) => (
               <View style={styles.tableRow} key={index}>
                 <Text style={styles.tableCol}>{content}</Text>
