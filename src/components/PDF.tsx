3e416eb0af487287daf14ace153598e8a9fbb263
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-12 13:19:05 +0900

    0712_1319

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -204,7 +204,7 @@ const PDF: FC = () => {
               <Text style={styles.tableColHeader}>単価</Text>
               <Text style={styles.tableColHeader}>金額</Text>
             </View>
-            {data.map((content: any, index: any) => (
+            {front_language1.map((content: any, index: any) => (
               <View style={styles.tableRow} key={index}>
                 <Text style={styles.tableCol}>{content}</Text>
                 <Text style={styles.tableCol}>{content}</Text>
