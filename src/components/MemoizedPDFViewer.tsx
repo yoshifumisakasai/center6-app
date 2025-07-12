b0f90466584f1e014f1b8c59da449022eda0b819
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-12 13:43:50 +0900

    0712_1343

--- src/components/MemoizedPDFViewer.tsx
+++ src/components/MemoizedPDFViewer.tsx
@@ -7,20 +7,18 @@ import { doc, getDoc } from "firebase/firestore";
 import { db } from "./FirebaseConfig";
 
 const [front_language1, setLanguage1] = useState([]);
-useEffect(() => {
-  const usersCollectionRef1 = doc(db, 'front_language', 'level_1');
 
-  getDoc(usersCollectionRef1).then((documentSnapshot) => {
-    if (documentSnapshot.exists()) {
-      // increment(documentSnapshot.get('content'))
-      // setLanguage1(documentSnapshot.get('content'))
-      setLanguage1(documentSnapshot.get('content'));
-      // console.log('countRefの中身は', countRef.current);
-    }
-  })
-});
 // PDFをPDFViewerというコンポーネントを利用してレンダリング
 const MemoizedPDFViewer: FC = () => {
+  useEffect(() => {
+    const usersCollectionRef1 = doc(db, 'front_language', 'level_1');
+
+    getDoc(usersCollectionRef1).then((documentSnapshot) => {
+      if (documentSnapshot.exists()) {
+        setLanguage1(documentSnapshot.get('content'));
+      }
+    })
+  });
   console.log('MemorizedPDFViewerコンポ実行')
   return (
     <div
