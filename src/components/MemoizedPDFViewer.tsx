a875a35d676e58bb961d439bd27541ab52123e32
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-12 13:41:41 +0900

    0712_1341

--- src/components/MemoizedPDFViewer.tsx
+++ src/components/MemoizedPDFViewer.tsx
@@ -6,19 +6,19 @@ import { PDFViewer } from "@react-pdf/renderer";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./FirebaseConfig";
 
-// const [front_language1, setLanguage1] = useState([]);
-// useEffect(() => {
-//   const usersCollectionRef1 = doc(db, 'front_language', 'level_1');
+const [front_language1, setLanguage1] = useState([]);
+useEffect(() => {
+  const usersCollectionRef1 = doc(db, 'front_language', 'level_1');
 
-//   getDoc(usersCollectionRef1).then((documentSnapshot) => {
-//     if (documentSnapshot.exists()) {
-//       // increment(documentSnapshot.get('content'))
-//       // setLanguage1(documentSnapshot.get('content'))
-//       setLanguage1(documentSnapshot.get('content'));
-//       // console.log('countRefの中身は', countRef.current);
-//     }
-//   })
-// });
+  getDoc(usersCollectionRef1).then((documentSnapshot) => {
+    if (documentSnapshot.exists()) {
+      // increment(documentSnapshot.get('content'))
+      // setLanguage1(documentSnapshot.get('content'))
+      setLanguage1(documentSnapshot.get('content'));
+      // console.log('countRefの中身は', countRef.current);
+    }
+  })
+});
 // PDFをPDFViewerというコンポーネントを利用してレンダリング
 const MemoizedPDFViewer: FC = () => {
   console.log('MemorizedPDFViewerコンポ実行')
