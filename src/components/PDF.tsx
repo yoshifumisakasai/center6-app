2cf88920ff1932acba4c0cf66b8edcc4109e2874
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-12 10:33:28 +0900

    0712_1033

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -136,8 +136,8 @@ const data = [
 const PDF = () => {
 
   // const param_d = props.front_language1;
-  // console.log('props内容(1)は', props);
-  // console.log('props内容(2)は', props.front_language1);
+  console.log('props内容(1)は');
+  console.log('props内容(2)は');
 
   // const isFirstRender = useRef(true);
   // let countRef = useRef([]);
