d2a1af9b9782d752e2fc44c0ebc6e472608a15d8
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-12 12:13:14 +0900

    0712_1213

--- src/components/MemoizedPDFViewer.tsx
+++ src/components/MemoizedPDFViewer.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import PDF from "./PDF";
 import { PDFViewer } from "@react-pdf/renderer";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./FirebaseConfig";
 
+const [front_language1, setLanguage1] = useState([]);
+useEffect(() => {
+  const usersCollectionRef1 = doc(db, 'front_language', 'level_1');
 
-
+  getDoc(usersCollectionRef1).then((documentSnapshot) => {
+    if (documentSnapshot.exists()) {
+      // increment(documentSnapshot.get('content'))
+      // setLanguage1(documentSnapshot.get('content'))
+      setLanguage1(documentSnapshot.get('content'));
+      // console.log('countRefの中身は', countRef.current);
+    }
+  })
+});
 // PDFをPDFViewerというコンポーネントを利用してレンダリング
-const MemoizedPDFViewer: FC = React.memo(() => {
+const MemoizedPDFViewer: FC = React.memo((front_language1) => {
   return (
     <div
       style={{
