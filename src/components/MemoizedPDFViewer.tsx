b0ca602ed2d1862599391bb6c083894c3248c75f
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-16 23:28:40 +0900

    0716_PDFレンダリング抑止

--- src/components/MemoizedPDFViewer.tsx
+++ src/components/MemoizedPDFViewer.tsx
@@ -10,17 +10,17 @@ import { db } from "./FirebaseConfig";
 // PDFをPDFViewerというコンポーネントを利用してレンダリング
 
 const MemoizedPDFViewer: FC = () => {
-  const [front_language1, setLanguage1] = useState([]);
+  // const [front_language1, setLanguage1] = useState([]);
 
-  useEffect(() => {
-    const usersCollectionRef1 = doc(db, 'front_language', 'level_1');
+  // useEffect(() => {
+  //   const usersCollectionRef1 = doc(db, 'front_language', 'level_1');
 
-    getDoc(usersCollectionRef1).then((documentSnapshot) => {
-      if (documentSnapshot.exists()) {
-        setLanguage1(documentSnapshot.get('content'));
-      }
-    })
-  },[]);
+  //   getDoc(usersCollectionRef1).then((documentSnapshot) => {
+  //     if (documentSnapshot.exists()) {
+  //       setLanguage1(documentSnapshot.get('content'));
+  //     }
+  //   })
+  // },[]);
   console.log('MemorizedPDFViewerコンポ実行')
   return (
     <div
