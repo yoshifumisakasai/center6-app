26c2a1c85cf441692adf84e81b21b734af9347fc
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-18 23:59:28 +0900

    0718_PDFレンダリング抑止19

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -204,7 +204,7 @@ const PDF: FC = () => {
               <Text style={styles.tableColHeader}>単価</Text>
               <Text style={styles.tableColHeader}>{shouldScrollToTop.current}</Text>
             </View>
-            {data[0].items.map((content: any, index: any) => (
+            {shouldScrollToTop.current.map((content: any, index: any) => (
               <View style={styles.tableRow} key={index}>
                 <Text style={styles.tableCol}>{content}</Text>
                 <Text style={styles.tableCol}>{content}</Text>
