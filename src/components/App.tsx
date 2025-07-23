13f0e80cd8adfc3c4866be263348d03a8e2740ec
yoshifumisakasai <put.ones.heart.into@gmail.com>
2025-07-23 23:01:11 +0900

    0723_2301(管理系コンポーネント追加）

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -29,9 +29,10 @@ import Kanri_FrontEnd from "./Kanri_FrontEnd";
 import Kanri_Skill_Check from "./Kanri_Skill_Check";
 import MemoizedPDFViewer from "./MemoizedPDFViewer";
 import React from "react";
+import Kanri_State from "./Kanri_State";
 const App: FC = () => {
     // const MemoPDFViewer = React.memo(MemoizedPDFViewer);
-    
+
     return (
         <ChakraProvider theme={theme}>
             <Flex w="100vw" h="100wh">
@@ -44,12 +45,13 @@ const App: FC = () => {
                                     <Route path={`/register/`} element={<Register />} />
                                     <Route path={`/`} element={<Login />} />
                                     <Route path={`/skill_Check/`} element={<Skill_Check />} />
-                                    <Route path={`/kanri_skill_Check/`} element={<Kanri_Skill_Check />} />
                                     <Route path={`/mypage`} element={<Mypage />} />
                                     <Route path={`/frontEnd/`} element={<FrontEnd />} />
+                                    <Route path={`/kanri_skill_Check/`} element={<Kanri_Skill_Check />} />
                                     <Route path={`/kanri_frontEnd/`} element={<Kanri_FrontEnd />} />
-                                    <Route path="/dashboard/" element={<Dashboard />} />
                                     <Route path="/kanri_dashboard/" element={<Kanri_Dashboard />} />
+                                    <Route path="/state/" element={<Kanri_State />} />
+                                    <Route path="/dashboard/" element={<Dashboard />} />
                                     <Route path="/state/" element={<State />} />
                                     <Route path="/framework/" element={<Framework />} />
                                     <Route path="/ratereview/" element={<RateReview />} />
---
+++ src/components/Kanri_State.tsx
@@ -0,0 +1,117 @@
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import Register from "./Register";
+import Login from "./Login";
+import Management from "./Management";
+import { FC, useState } from "react";
+import Mypage from "./Mypage";
+import Skill_Check from "./Skill_Check";
+import Framework from "./Framework";
+import Language from "./Language";
+import FrontEndResult from "./FrontEndResult";
+import Graph from "./Graph"
+import Account from "./State";
+import { SideMenu } from "./SideMenu";
+import { Box, Button, ChakraProvider, Checkbox, CheckboxGroup, Flex, Tab, TabList, TabPanel, TabPanels, Tabs, theme } from "@chakra-ui/react";
+import { TopHeader } from "./TopHeader";
+import Charts from "./Hook";
+import { MailBox } from "./MailBox";
+import { Message } from "./Message";
+import SideMenu_temp from "./SideMenu_temp";
+import { TodoTitle } from "./TodoTitle";
+import { AddIcon } from "@chakra-ui/icons";
+import SideMenu_top from "./SideMenu_top";
+import Firebase_db from "./Firebase_db_level3";
+import Firebase_db_level2 from "./Firebase_db_level2";
+import Firebase_db_level3 from "./Firebase_db_level3";
+import Firebase_db_level1 from "./Firebase_db_level1";
+import { DocumentData } from "firebase/firestore";
+import Kanri_SideMenu_temp from "./Kanri_SideMenu_temp";
+import Kanri_Firebase_db_level1 from "./Kanri_Firebase_db_level1";
+import Kanri_Firebase_db_level3 from "./Kanri_Firebase_db_level3";
+import Kanri_Firebase_db_level2 from "./Kanri_Firebase_db_level2";
+const Kanri_State: FC = () => {
+    let navigate = useNavigate();
+    let goBack = () => {
+        navigate(-1);
+    };
+    const [parentState, setParentState] = useState<DocumentData>([]);
+    const handleValueChange1 = (newValue: any) => {
+        set_level_1_Form(newValue);
+    };
+
+    const handleValueChange2 = (newValue: any) => {
+        set_level_2_Form(newValue);
+    };
+
+    const handleValueChange3 = (newValue: any) => {
+        set_level_3_Form(newValue);
+    };
+
+    const [level_1, set_level_1_Form] = useState<any>([]);
+    const [level_2, set_level_2_Form] = useState<any>([]);
+    const [level_3, set_level_3_Form] = useState<any>([]);
+
+    const next = () => {
+        navigate("/frontEndResult/", { state: entity })
+    }
+
+    interface HomeEntity {
+        level1: {},
+        level2: {},
+        level3: {}
+    }
+
+    const entity: HomeEntity = {
+        level1: level_1,
+        level2: level_2,
+        level3: level_3
+    };
+
+    return (
+        <>
+            <ChakraProvider theme={theme}>
+                <Flex w="100vw" h="100wh" >
+                    <SideMenu_top />
+                    <Kanri_SideMenu_temp />
+
+                    <Flex direction="column">
+                        <TodoTitle
+                            title="Language"
+                            as="h1"
+                            fontSize={{ base: "xl", md: "xl" }}
+                            mt="1"
+                        />
+                        <Tabs>
+                            <TabList mb='1em'>
+                                <Tab>Level1</Tab>
+                                <Tab>Level2</Tab>
+                                <Tab>Level3</Tab>
+                            </TabList>
+                            <TabPanels>
+
+                                <TabPanel>
+                                    <Kanri_Firebase_db_level1 />
+                                </TabPanel>
+
+
+                                <TabPanel>
+                                    <Kanri_Firebase_db_level2 />
+                                </TabPanel>
+
+
+                                <TabPanel>
+                                    <Kanri_Firebase_db_level3 />
+                                </TabPanel>
+
+                            </TabPanels>
+                            
+                        </Tabs>
+                    </Flex>
+                </Flex>
+            </ChakraProvider>
+
+        </>
+    );
+};
+
+export default Kanri_State;
